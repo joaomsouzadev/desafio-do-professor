9261bea426894040f4d1db50ad62db726b2d44d1
joaomsouzadev <joao.msouzadev@gmail.com>
2024-02-17 03:06:07 -0300

    projeto finalizado

--- src/index.js
+++ src/index.js
@@ -1,40 +1,47 @@
 let nome = "João";
-let xp = 1000;
+let xp = 10001;
 let nivel = "";
 
-switch (xp <= 1000) {
-    case xp <= 1000: 
-        nivel = "Ferro";
-        break;
+switch (true) {
 
-    case xp >= 1001 && xp <= 2000 === true: 
-        nivel = "Bronze";
-        break;
+  case xp <= 1000:
+    nivel = "Ferro";
+    console.log("O herói " + nome + " está no nível: " + nivel);
+    break;
 
-    case xp >= 2001 && xp <= 5000 === true: 
-        nivel = "Prata";
-        break;
+  case xp >= 1001 && xp <= 2000:
+    nivel = "Bronze";
+    console.log("O herói " + nome + " está no nível: " + nivel);
+    break;
 
-    case xp >= 6001 && xp <= 7000 === true: 
-        nivel = "Ouro";
-        break;
+  case xp >= 2001 && xp <= 5000:
+    nivel = "Prata";
+    console.log("O herói " + nome + " está no nível: " + nivel);
+    break;
 
-    case xp >= 7001 && xp <= 8000 === true: 
-        nivel = "Platina";
-        break;
+  case xp >= 6001 && xp <= 7000:
+    nivel = "Ouro";
+    console.log("O herói " + nome + " está no nível: " + nivel);
+    break;
 
-    case xp >= 8001 && xp <= 9000 === true: 
-        nivel = "Ascendente";
-        break;
+  case xp >= 7001 && xp <= 8000:
+    nivel = "Platina";
+    console.log("O herói " + nome + " está no nível: " + nivel);
+    break;
 
-    case xp >= 9001 && xp <= 10000 === true: 
-        nivel = "Imortal";
-        break;
+  case xp >= 8001 && xp <= 9000:
+    nivel = "Ascendente";
+    console.log("O herói " + nome + " está no nível: " + nivel);
+    break;
 
-    case xp >= 10001: 
-        nivel = "Radiante";
-        break;
-        
+  case xp >= 9001 && xp <= 10000:
+    nivel = "Imortal";
+    console.log("O herói " + nome + " está no nível: " + nivel);
+    break;
+
+  case xp >= 10001:
+    nivel = "Radiante";
+    console.log("O herói " + nome + " está no nível: " + nivel);
+    break;
+
 }
-
-console.log("O herói " + nome + " está no nível: " + nivel)
